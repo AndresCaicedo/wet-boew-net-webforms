7e8ecea67663f250f9e0a346dd40dff8d552b149
Andres Caicedo <AndresCaicedo>
2012-09-24 15:59:22 -0700

    Using basic content page.

--- wet-boew-net-webforms/Default.aspx.cs
+++ wet-boew-net-webforms/Default.aspx.cs
@@ -11,7 +11,7 @@ namespace wet_boew_net_webforms
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            Response.Redirect("WetIntranet.aspx");
         }
     }
 }
