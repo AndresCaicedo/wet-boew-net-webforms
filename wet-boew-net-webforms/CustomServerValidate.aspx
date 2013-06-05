9aaa9151cef30171a5ecd1cdab24575e3ab359c3
Andres Caicedo <AndresCaicedo>
2013-06-05 06:54:39 -0700

    Moved from /build to /dist, upgraded 3.0.7

--- wet-boew-net-webforms/CustomServerValidate.aspx
+++ wet-boew-net-webforms/CustomServerValidate.aspx
@@ -4,7 +4,7 @@
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
 <div class="wet-boew-formvalid">
 <asp:ValidationSummary runat="server" ID="SuperValidationSummary" CssClass="errorContainer" HeaderText="<p>There were errors encountered:</p>" />
-<asp:Label runat="server" ID="SampleLabel" AssociatedControlID="SampleTextBox">Important Data (type &quot;cool&quot;)
+<asp:Label runat="server" ID="SampleLabel" AssociatedControlID="SampleTextBox">Important Data (correct answer is &quot;cool&quot;)
 <strong class="error">
 <asp:CustomValidator runat="server" ID="MyCustomValidator" 
     ControlToValidate="SampleTextBox" 
---
+++ wet-boew-net-webforms/dist/js/settings.js
@@ -0,0 +1,27 @@
+/*!
+ *
+ * Web Experience Toolkit (WET) / Boîte à outils de l'expérience Web (BOEW)
+ * wet-boew.github.io/wet-boew/License-eng.txt / wet-boew.github.io/wet-boew/Licence-fra.txt
+ *
+ * Version: v3.0.7-release Build: 2013-05-16 04:34 AM
+ *
+ */
+/*
+WET-BOEW-Settings
+ */
+var wet_boew_properties = {
+	/** global plugins are called via a array of dependency names **/
+	globals : ['equalize', 'deselectradio', 'css3ie']
+};
+
+/*
+Global overrides for individual components
+ */
+
+// Share widget
+var wet_boew_share = {
+	sites : ['del.icio.us', 'digg', 'facebook', 'google', 'linkedin', 'reddit', 'stumbleupon', 'twitter', 'yahoobuzz']
+};
+
+// Charts widget 
+// var wet_boew_charts = { };
